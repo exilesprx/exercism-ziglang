ca76442cfbe0f62abfc5a7a5152ebb51dea46ce3
Exiles.prx <exiles.prx@gmail.com>
2024-03-17 23:43:25 -0400

    Completed collatz conjecture

--- collatz-conjecture/collatz_conjecture.zig
+++ collatz-conjecture/collatz_conjecture.zig
@@ -1,6 +1,24 @@
 // Please implement the `ComputationError.IllegalArgument` error.
+pub const ComputationError = error{
+    IllegalArgument,
+};
 
 pub fn steps(number: usize) anyerror!usize {
-    _ = number;
-    @compileError("please implement the steps function");
+    if (number == 0) {
+        return ComputationError.IllegalArgument;
+    }
+
+    var num: usize = number;
+    var i: usize = 0;
+    while (num != 1) {
+        if (num % 2 == 0) {
+            num /= 2;
+            i += 1;
+            continue;
+        }
+
+        num = 3 * num + 1;
+        i += 1;
+    }
+    return i;
 }
