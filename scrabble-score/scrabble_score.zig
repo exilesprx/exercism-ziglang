6b2dc7aef7a231f2fff858ea4e4ae918fb07cfbf
Exiles.prx <exiles.prx@gmail.com>
2024-03-26 22:50:22 -0400

    Solved scrabble score

--- scrabble-score/scrabble_score.zig
+++ scrabble-score/scrabble_score.zig
@@ -1,4 +1,33 @@
+const std = @import("std");
+const Letters = struct { letters: []const u8, points: u8 };
+
+const ones = Letters{ .letters = "AEIOULNRST", .points = 1 };
+const twos = Letters{ .letters = "DG", .points = 2 };
+const threes = Letters{ .letters = "BCMP", .points = 3 };
+const fours = Letters{ .letters = "FHVWY", .points = 4 };
+const fives = Letters{ .letters = "K", .points = 5 };
+const eights = Letters{ .letters = "JX", .points = 8 };
+const tens = Letters{ .letters = "QZ", .points = 10 };
+
 pub fn score(s: []const u8) u32 {
-    _ = s;
-    @compileError("please implement the score function");
+    var sum: u32 = 0;
+    const letters = [_]Letters{ ones, twos, threes, fours, fives, eights, tens };
+    for (s) |c| {
+        for (letters) |l| {
+            if (contains(l.letters, c)) {
+                sum += l.points;
+            }
+        }
+    }
+
+    return sum;
+}
+
+fn contains(letters: []const u8, letter: u8) bool {
+    for (letters) |l| {
+        if (std.ascii.isAlphabetic(letter) and std.ascii.toUpper(letter) == l) {
+            return true;
+        }
+    }
+    return false;
 }
