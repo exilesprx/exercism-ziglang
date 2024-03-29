59075f616456f7c2936788c5b6867e252bbec3ad
Exiles.prx <exiles.prx@gmail.com>
2024-03-28 22:41:49 -0400

    Found community solution which I like

--- pangram/pangram.zig
+++ pangram/pangram.zig
@@ -3,27 +3,24 @@ const print = std.debug.print;
 
 pub fn isPangram(str: []const u8) bool {
     var alphabet: [26]u8 = undefined;
-    var uniqueChars: usize = 0;
     var i: usize = 0;
     for (str) |c| {
-        if (std.ascii.isAlphabetic(c) and !contains(&alphabet, c)) {
-            alphabet[i] = c;
+        if (std.ascii.isAlphabetic(c)) {
+            // fill the alphabet array with 1s if the letter is found
+            // and the index is the letter's position in the alphabet
+            alphabet[std.ascii.toLower(c) - 'a'] = 1; // c - a = 99 - 97 = 2
             i += 1;
-            uniqueChars += 1;
         }
     }
 
-    return uniqueChars == alphabet.len;
+    return foundAll(&alphabet);
 }
 
-fn contains(s: *[26]u8, c: u8) bool {
+fn foundAll(s: *[26]u8) bool {
     for (s) |x| {
-        if (!std.ascii.isAlphabetic(x)) {
-            continue;
-        }
-        if (std.ascii.toLower(x) == std.ascii.toLower(c)) {
-            return true;
+        if (x != 1) {
+            return false;
         }
     }
-    return false;
+    return true;
 }
