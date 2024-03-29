db3b18e6dbc5548ccf9bfb90962b3d2860e26def
Exiles.prx <exiles.prx@gmail.com>
2024-03-28 22:16:17 -0400

    Solved exercise pangram

--- pangram/pangram.zig
+++ pangram/pangram.zig
@@ -1,4 +1,29 @@
+const std = @import("std");
+const print = std.debug.print;
+
 pub fn isPangram(str: []const u8) bool {
-    _ = str;
-    @compileError("please implement the isPangram function");
+    var alphabet: [26]u8 = undefined;
+    var uniqueChars: usize = 0;
+    var i: usize = 0;
+    for (str) |c| {
+        if (std.ascii.isAlphabetic(c) and !contains(&alphabet, c)) {
+            alphabet[i] = c;
+            i += 1;
+            uniqueChars += 1;
+        }
+    }
+
+    return uniqueChars == alphabet.len;
+}
+
+fn contains(s: *[26]u8, c: u8) bool {
+    for (s) |x| {
+        if (!std.ascii.isAlphabetic(x)) {
+            continue;
+        }
+        if (std.ascii.toLower(x) == std.ascii.toLower(c)) {
+            return true;
+        }
+    }
+    return false;
 }
