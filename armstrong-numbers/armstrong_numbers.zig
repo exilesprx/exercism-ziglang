caccf4df0b627498c472c0d65e99bf3a32f60300
Exiles.prx <exiles.prx@gmail.com>
2024-04-13 13:09:54 -0400

    WIP

--- armstrong-numbers/armstrong_numbers.zig
+++ armstrong-numbers/armstrong_numbers.zig
@@ -1,4 +1,14 @@
+const std = @import("std");
+const print = std.debug.print;
+
 pub fn isArmstrongNumber(num: u128) bool {
     _ = num;
-    @compileError("please implement the isArmstrongNumber function");
+    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    defer arena.deinit();
+
+    const buf = arena.allocator();
+    const str: []const u128 = std.fmt.allocPrint(buf, "{d}", .{102});
+    defer buf.free(str);
+
+    print("str: {}\n", str);
 }
