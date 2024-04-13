cd23693223e22f122127eb151282cae99f7c8555
Exiles.prx <exiles.prx@gmail.com>
2024-04-13 14:52:15 -0400

    Completed exercise armstrong numbers

--- armstrong-numbers/armstrong_numbers.zig
+++ armstrong-numbers/armstrong_numbers.zig
@@ -2,13 +2,14 @@ const std = @import("std");
 const print = std.debug.print;
 
 pub fn isArmstrongNumber(num: u128) bool {
-    _ = num;
-    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
-    defer arena.deinit();
+    const allocator = std.heap.page_allocator;
+    const str = std.fmt.allocPrint(allocator, "{d}", .{num}) catch "Failed to format";
 
-    const buf = arena.allocator();
-    const str: []const u128 = std.fmt.allocPrint(buf, "{d}", .{102});
-    defer buf.free(str);
+    var sum: u128 = 0;
+    for (str) |c| {
+        const digit: u8 = std.fmt.charToDigit(c, 10) catch 0;
+        sum += std.math.pow(u128, digit, @as(u128, str.len));
+    }
 
-    print("str: {}\n", str);
+    return num == sum;
 }
