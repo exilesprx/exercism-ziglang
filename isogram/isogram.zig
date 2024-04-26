422afaa8a19c31f36cfa2d2a84a037238a99af5a
Exiles.prx <exiles.prx@gmail.com>
2024-04-25 22:41:51 -0400

    Completed exercise isogram

--- isogram/isogram.zig
+++ isogram/isogram.zig
@@ -1,4 +1,18 @@
+const std = @import("std");
+
 pub fn isIsogram(str: []const u8) bool {
-    _ = str;
-    @compileError("please implement the isIsogram function");
+    var letters: [26]u8 = std.mem.zeroes([26]u8);
+    for (str) |c| {
+        if (!std.ascii.isAlphabetic(c)) {
+            continue;
+        }
+
+        letters[std.ascii.toLower(c) - 'a'] += 1;
+
+        if (letters[std.ascii.toLower(c) - 'a'] > 1) {
+            return false;
+        }
+    }
+
+    return true;
 }
