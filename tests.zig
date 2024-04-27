21912fffd6af529966b4f64cef99e990e4281df3
Exiles.prx <exiles.prx@gmail.com>
2024-04-26 22:21:31 -0400

    Tests step
    Adding test step to run all exercism tests

---
+++ tests.zig
@@ -0,0 +1,15 @@
+const std = @import("std");
+
+comptime {
+    _ = @import("armstrong-numbers/test_armstrong_numbers.zig");
+    _ = @import("collatz-conjecture/test_collatz_conjecture.zig");
+    _ = @import("difference-of-squares/test_difference_of_squares.zig");
+    _ = @import("isogram/test_isogram.zig");
+    _ = @import("leap/test_leap.zig");
+    _ = @import("pangram/test_pangram.zig");
+    _ = @import("scrabble-score/test_scrabble_score.zig");
+}
+
+test {
+    std.testing.refAllDecls(@This());
+}
