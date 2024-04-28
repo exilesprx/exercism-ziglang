35410bf2b7c049c2083d1add6e4c0002c03546cd
Exiles.prx <exiles.prx@gmail.com>
2024-04-27 22:34:19 -0400

    Completed exercise hamming

--- hamming/hamming.zig
+++ hamming/hamming.zig
@@ -1,5 +1,23 @@
+const std = @import("std");
+const print = std.debug.print;
+
+pub const DnaError = error{ EmptyDnaStrands, UnequalDnaStrands };
+
 pub fn compute(first: []const u8, second: []const u8) DnaError!usize {
-    _ = first;
-    _ = second;
-    @compileError("please implement the compute function");
+    if (first.len == 0 or second.len == 0) {
+        return DnaError.EmptyDnaStrands;
+    }
+
+    if (first.len != second.len) {
+        return DnaError.UnequalDnaStrands;
+    }
+
+    var hamming: usize = 0;
+    for (first, 0..) |c, i| {
+        if (c != second[i]) {
+            hamming += 1;
+        }
+    }
+
+    return hamming;
 }
