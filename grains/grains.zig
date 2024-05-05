47a472bf5f1718a6f0d7cafef08a5e7361d2e29b
Exiles.prx <exiles.prx@gmail.com>
2024-05-05 19:18:58 -0400

    Solved exercise grains

--- grains/grains.zig
+++ grains/grains.zig
@@ -1,8 +1,24 @@
+const std = @import("std");
+
+pub const ChessboardError = error{IndexOutOfBounds};
+
 pub fn square(index: usize) ChessboardError!u64 {
-    _ = index;
-    @compileError("please implement the square function");
+    if (index <= 0 or index >= 65) {
+        return ChessboardError.IndexOutOfBounds;
+    }
+
+    var sum: u64 = 1;
+    var i: u8 = 1;
+    while (i < index) : (i += 1) {
+        sum = sum * 2;
+    }
+    return sum;
 }
 
 pub fn total() u64 {
-    @compileError("please implement the total function");
+    var sum: u64 = 0;
+    for (1..65) |i| {
+        sum += square(i) catch 0;
+    }
+    return sum;
 }
