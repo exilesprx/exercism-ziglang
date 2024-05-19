06dc2bcf6ac2b6354c6aec42b832bd01004cfc47
Exiles.prx <exiles.prx@gmail.com>
2024-05-18 20:10:38 -0400

    Solver exercise isbn verifier

--- isbn-verifier/isbn_verifier.zig
+++ isbn-verifier/isbn_verifier.zig
@@ -1,4 +1,31 @@
+const std = @import("std");
+
 pub fn isValidIsbn10(s: []const u8) bool {
-    _ = s;
-    @compileError("please implement the isValidIsbn10 function");
+    var digits: usize = 0;
+    var sum: usize = 0;
+    for (s) |c| {
+        if (c == '-') {
+            continue;
+        }
+
+        if (std.ascii.isAlphabetic(c) and std.ascii.toUpper(c) != 'X') {
+            return false;
+        }
+
+        var value: usize = undefined;
+        if (std.ascii.toUpper(c) == 'X') {
+            if (digits != 9) {
+                return false;
+            }
+
+            value = 10;
+        } else {
+            value = std.fmt.charToDigit(c, 10) catch 0;
+        }
+
+        sum += value * (10 - digits);
+        digits += 1;
+    }
+
+    return digits == 10 and sum % 11 == 0;
 }
