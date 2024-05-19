e1fc5043599ad7682085b55fe81e6ad5fc579dfb
Exiles.prx <exiles.prx@gmail.com>
2024-05-19 11:49:34 -0400

    Leverage switch matching instead

--- isbn-verifier/isbn_verifier.zig
+++ isbn-verifier/isbn_verifier.zig
@@ -4,23 +4,16 @@ pub fn isValidIsbn10(s: []const u8) bool {
     var digits: usize = 0;
     var sum: usize = 0;
     for (s) |c| {
-        if (c == '-') {
-            continue;
-        }
-
-        if (std.ascii.isAlphabetic(c) and std.ascii.toUpper(c) != 'X') {
-            return false;
-        }
-
         var value: usize = undefined;
-        if (std.ascii.toUpper(c) == 'X') {
-            if (digits != 9) {
-                return false;
-            }
-
-            value = 10;
-        } else {
-            value = std.fmt.charToDigit(c, 10) catch 0;
+        switch (c) {
+            '-' => continue,
+            'X' => if (digits == 9) {
+                value = 10;
+            } else return false,
+            '0'...'9' => {
+                value = std.fmt.charToDigit(c, 10) catch 0;
+            },
+            else => return false,
         }
 
         sum += value * (10 - digits);
