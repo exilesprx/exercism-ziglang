8316c48607b342a55d2c13f1f8bb4d826a46306c
Exiles.prx <exiles.prx@gmail.com>
2024-05-21 22:53:20 -0400

    Updated version to use built-in methods

--- resistor-color/resistor_color.zig
+++ resistor-color/resistor_color.zig
@@ -1,21 +1,11 @@
+const std = @import("std");
+
 pub const ColorBand = enum { black, brown, red, orange, yellow, green, blue, violet, grey, white };
 
 pub fn colorCode(color: ColorBand) usize {
-    return switch (color) {
-        .black => 0,
-        .brown => 1,
-        .red => 2,
-        .orange => 3,
-        .yellow => 4,
-        .green => 5,
-        .blue => 6,
-        .violet => 7,
-        .grey => 8,
-        .white => 9,
-    };
+    return @intFromEnum(color);
 }
 
 pub fn colors() []const ColorBand {
-    const c: [10]ColorBand = .{ ColorBand.black, ColorBand.brown, ColorBand.red, ColorBand.orange, ColorBand.yellow, ColorBand.green, ColorBand.blue, ColorBand.violet, ColorBand.grey, ColorBand.white };
-    return &c;
+    return std.enums.values(ColorBand);
 }
