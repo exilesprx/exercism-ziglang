96a39a29b805e94f8805c44a0b006dab457952da
Exiles.prx <exiles.prx@gmail.com>
2024-05-21 22:47:07 -0400

    Solved exercise resistor color

--- resistor-color/resistor_color.zig
+++ resistor-color/resistor_color.zig
@@ -1,7 +1,21 @@
+pub const ColorBand = enum { black, brown, red, orange, yellow, green, blue, violet, grey, white };
+
 pub fn colorCode(color: ColorBand) usize {
-    @compileError("determine the value of a colorband on a resistor");
+    return switch (color) {
+        .black => 0,
+        .brown => 1,
+        .red => 2,
+        .orange => 3,
+        .yellow => 4,
+        .green => 5,
+        .blue => 6,
+        .violet => 7,
+        .grey => 8,
+        .white => 9,
+    };
 }
 
 pub fn colors() []const ColorBand {
-    @compileError("refer to a collection of all resistor colorbands");
+    const c: [10]ColorBand = .{ ColorBand.black, ColorBand.brown, ColorBand.red, ColorBand.orange, ColorBand.yellow, ColorBand.green, ColorBand.blue, ColorBand.violet, ColorBand.grey, ColorBand.white };
+    return &c;
 }
