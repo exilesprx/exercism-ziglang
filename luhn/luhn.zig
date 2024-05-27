246381e4c1ba1057aad49949f9356a151b546fb0
Exiles.prx <exiles.prx@gmail.com>
2024-05-27 13:47:14 -0400

    Solved exercise luhn

--- luhn/luhn.zig
+++ luhn/luhn.zig
@@ -1,4 +1,30 @@
+const std = @import("std");
+
 pub fn isValid(s: []const u8) bool {
-    _ = s;
-    @compileError("please implement the isValid function");
+    var sum: usize = 0;
+    var digitCount: usize = 0;
+    var i: usize = s.len;
+    while (i > 0) : (i -= 1) {
+        switch (s[i - 1]) {
+            '0'...'9' => {
+                digitCount += 1;
+                const digit: u8 = std.fmt.charToDigit(s[i - 1], 10) catch unreachable;
+                if (digitCount % 2 != 0) {
+                    sum += digit;
+                    continue;
+                }
+
+                const val: usize = digit * 2;
+                if (val > 9) {
+                    sum += val - 9;
+                    continue;
+                }
+                sum += val;
+            },
+            ' ' => continue,
+            else => return false,
+        }
+    }
+
+    return digitCount > 1 and sum % 10 == 0;
 }
