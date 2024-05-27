fe7e94ae570414b2ddb44d707e4401a2c517567c
Exiles.prx <exiles.prx@gmail.com>
2024-05-26 22:05:12 -0400

    Solved exercise resistor color duo

--- resistor-color-duo/resistor_color_duo.zig
+++ resistor-color-duo/resistor_color_duo.zig
@@ -1,4 +1,14 @@
+pub const ColorBand = enum { black, brown, red, orange, yellow, green, blue, violet, grey, white };
+
 pub fn colorCode(colors: [2]ColorBand) usize {
-    _ = colors;
-    @compileError("please implement the colorCode function");
+    var output: usize = 0;
+    for (colors, 0..) |color, i| {
+        var value: usize = @intFromEnum(color);
+        if (i == 0) {
+            value *= 10;
+        }
+        output += value;
+    }
+
+    return output;
 }
