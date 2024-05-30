9e30a0d82986ddd0bedbd75e76e757864151e596
Exiles.prx <exiles.prx@gmail.com>
2024-05-29 20:44:30 -0400

    Solved exercise darts

--- darts/darts.zig
+++ darts/darts.zig
@@ -1,13 +1,24 @@
+const std = @import("std");
+
 pub const Coordinate = struct {
-    // This struct, as well as its fields and methods, needs to be implemented.
+    x: f32,
+    y: f32,
 
     pub fn init(x_coord: f32, y_coord: f32) Coordinate {
-        _ = x_coord;
-        _ = y_coord;
-        @compileError("please implement the init method");
+        return Coordinate{ .x = x_coord, .y = y_coord };
     }
     pub fn score(self: Coordinate) usize {
-        _ = self;
-        @compileError("please implement the score method");
+        const radius: f32 = std.math.sqrt(std.math.pow(f32, self.x, 2) + std.math.pow(f32, self.y, 2));
+        if (radius <= 1.0) {
+            return 10;
+        }
+        if (radius <= 5.0) {
+            return 5;
+        }
+
+        if (radius <= 10.0) {
+            return 1;
+        }
+        return 0;
     }
 };
