8b3f4499246ebe6529039c4e1c1b256bf01848cc
Exiles.prx <exiles.prx@gmail.com>
2024-05-31 23:54:40 -0400

    Update test suite

--- tests.zig
+++ tests.zig
@@ -3,12 +3,19 @@ const std = @import("std");
 comptime {
     _ = @import("armstrong-numbers/test_armstrong_numbers.zig");
     _ = @import("collatz-conjecture/test_collatz_conjecture.zig");
+    _ = @import("darts/test_darts.zig");
     _ = @import("difference-of-squares/test_difference_of_squares.zig");
+    _ = @import("grains/test_grains.zig");
+    _ = @import("hamming/test_hamming.zig");
+    _ = @import("isbn-verifier/test_isbn_verifier.zig");
     _ = @import("isogram/test_isogram.zig");
     _ = @import("leap/test_leap.zig");
+    _ = @import("luhn/test_luhn.zig");
     _ = @import("pangram/test_pangram.zig");
+    _ = @import("resistor-color/test_resistor_color.zig");
+    _ = @import("resistor-color-duo/test_resistor_color_duo.zig");
     _ = @import("scrabble-score/test_scrabble_score.zig");
-    _ = @import("hamming/test_hamming.zig");
+    _ = @import("triangle/test_triangle.zig");
 }
 
 test {
