a7e3b0abce90d242df9e58f7dd4917dc6de6459a
Exiles.prx <exiles.prx@gmail.com>
2024-07-14 19:49:57 -0400

    Solved exercise space age

--- space-age/space_age.zig
+++ space-age/space_age.zig
@@ -1,9 +1,27 @@
+const std = @import("std");
+const print = std.debug.print;
 pub const Planet = enum {
     mercury,
+    venus,
+    earth,
+    mars,
+    jupiter,
+    saturn,
+    uranus,
+    neptune,
 
     pub fn age(self: Planet, seconds: usize) f64 {
-        _ = self;
-        _ = seconds;
-        @compileError("please implement the age method");
+        const earthYears: f64 = switch (self) {
+            Planet.mercury => 0.2408467,
+            Planet.venus => 0.61519726,
+            Planet.earth => 1.0,
+            Planet.mars => 1.8808158,
+            Planet.jupiter => 11.862615,
+            Planet.saturn => 29.447498,
+            Planet.uranus => 84.016846,
+            Planet.neptune => 164.79132,
+        };
+
+        return @as(f64, @floatFromInt(seconds)) / (31557600.0 * earthYears);
     }
 };
