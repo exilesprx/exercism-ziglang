160aaef16969e4dd5aa9ff4b85e38bc4235a7f31
Exiles.prx <exiles.prx@gmail.com>
2024-08-22 00:40:05 -0400

    Solved exercise perfect numbers

--- perfect-numbers/perfect_numbers.zig
+++ perfect-numbers/perfect_numbers.zig
@@ -1,3 +1,6 @@
+const std = @import("std");
+const assert = std.debug.assert;
+
 pub const Classification = enum {
     deficient,
     perfect,
@@ -6,6 +9,21 @@ pub const Classification = enum {
 
 /// Asserts that `n` is nonzero.
 pub fn classify(n: u64) Classification {
-    _ = n;
-    @compileError("please implement the classify function");
+    assert(n != 0);
+
+    var i: u64 = 1;
+    var aliquot_sum: u128 = 0;
+    while (i < n) : (i += 1) {
+        if (n % i == 0) {
+            aliquot_sum += i;
+        }
+    }
+
+    if (n < aliquot_sum) {
+        return Classification.abundant;
+    } else if (n > aliquot_sum) {
+        return Classification.deficient;
+    }
+
+    return Classification.perfect;
 }
