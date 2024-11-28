2ed5abd931bf57d52f7a13c735baf3fe3b7af88e
Exiles.prx <exiles.prx@gmail.com>
2024-11-27 21:28:45 -0500

    Solved exercise rotational cipher

--- rotational-cipher/rotational_cipher.zig
+++ rotational-cipher/rotational_cipher.zig
@@ -1,6 +1,18 @@
+const std = @import("std");
+const mem = std.mem;
+
 pub fn rotate(allocator: mem.Allocator, text: []const u8, shiftKey: u5) mem.Allocator.Error![]u8 {
-    _ = allocator;
-    _ = text;
-    _ = shiftKey;
-    @compileError("please implement the rotate function");
+    const cipher: []u8 = try allocator.alloc(u8, text.len);
+    for (text, 0..) |c, i| {
+        if (c >= 'a' and c <= 'z') {
+            cipher[i] = (((c - 'a') + shiftKey) % 26) + 'a';
+            continue;
+        }
+        if (c >= 'A' and c <= 'Z') {
+            cipher[i] = (((c - 'A') + shiftKey) % 26) + 'A';
+            continue;
+        }
+        cipher[i] = c;
+    }
+    return cipher;
 }
