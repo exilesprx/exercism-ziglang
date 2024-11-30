b891d248c6a5eca291130a2294882b16faa9a6f4
Exiles.prx <exiles.prx@gmail.com>
2024-11-29 23:37:34 -0500

    Solved exercise reverse string

--- reverse-string/reverse_string.zig
+++ reverse-string/reverse_string.zig
@@ -1,6 +1,8 @@
 /// Writes a reversed copy of `s` to `buffer`.
 pub fn reverse(buffer: []u8, s: []const u8) []u8 {
-    _ = buffer;
-    _ = s;
-    @compileError("please implement the reverse function");
+    for (s, 0..) |c, i| {
+        buffer[s.len - i - 1] = c;
+    }
+
+    return buffer;
 }
