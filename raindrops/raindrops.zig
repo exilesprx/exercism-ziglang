4096057c3ec0788b05a8cc11892532187c9ec740
Exiles.prx <exiles.prx@gmail.com>
2025-09-17 10:42:37 -0400

    fix: solved exercise rain drops

--- raindrops/raindrops.zig
+++ raindrops/raindrops.zig
@@ -1,5 +1,26 @@
+const std = @import("std");
+
 pub fn convert(buffer: []u8, n: u32) []const u8 {
-    _ = buffer;
-    _ = n;
-    @compileError("please implement the convert function");
+    var pos: usize = 0;
+
+    if (n % 3 == 0) {
+        @memcpy(buffer[pos .. pos + 5], "Pling");
+        pos += 5;
+    }
+
+    if (n % 5 == 0) {
+        @memcpy(buffer[pos .. pos + 5], "Plang");
+        pos += 5;
+    }
+
+    if (n % 7 == 0) {
+        @memcpy(buffer[pos .. pos + 5], "Plong");
+        pos += 5;
+    }
+
+    if (pos != 0) {
+        return buffer[0..pos];
+    }
+
+    return std.fmt.bufPrint(buffer, "{}", .{n}) catch unreachable;
 }
