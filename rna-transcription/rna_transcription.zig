0bb729a2c60e6010e93e7509bdf543c18634d468
Exiles.prx <exiles.prx@gmail.com>
2025-09-17 12:21:26 -0400

    fix: use try

--- rna-transcription/rna_transcription.zig
+++ rna-transcription/rna_transcription.zig
@@ -2,7 +2,7 @@ const std = @import("std");
 const mem = std.mem;
 
 pub fn toRna(allocator: mem.Allocator, dna: []const u8) mem.Allocator.Error![]const u8 {
-    var rna = std.ArrayList(u8).initCapacity(allocator, dna.len) catch unreachable;
+    var rna = try std.ArrayList(u8).initCapacity(allocator, dna.len);
 
     for (dna) |nucleotide| {
         switch (nucleotide) {
