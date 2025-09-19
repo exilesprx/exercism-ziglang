aa376d748c241241fe41dd0207c15bcbbd230fb8
Exiles.prx <exiles.prx@gmail.com>
2025-09-19 14:44:55 -0400

    fix: solved sum of multiples

--- sum-of-multiples/sum_of_multiples.zig
+++ sum-of-multiples/sum_of_multiples.zig
@@ -2,8 +2,25 @@ const std = @import("std");
 const mem = std.mem;
 
 pub fn sum(allocator: mem.Allocator, factors: []const u32, limit: u32) !u64 {
-    _ = allocator;
-    _ = factors;
-    _ = limit;
-    @compileError("please implement the sum function");
+    var earnedEnergy = std.AutoHashMap(u64, void).init(allocator);
+    defer earnedEnergy.deinit();
+
+    for (factors) |factor| {
+        if (factor == 0 or factor > limit) {
+            continue;
+        }
+
+        var i = factor;
+        while (i < limit) : (i += factor) {
+            try earnedEnergy.put(i, {});
+        }
+    }
+
+    var energyTotal: u64 = 0;
+    var items = earnedEnergy.keyIterator();
+    while (items.next()) |points| {
+        energyTotal += points.*;
+    }
+
+    return energyTotal;
 }
