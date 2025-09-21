0ed0cd1638a027a3e3f3670971dedbda09f2a64f
Exiles.prx <exiles.prx@gmail.com>
2025-09-20 22:53:56 -0400

    fix: test coverage

--- tests.zig
+++ tests.zig
@@ -31,6 +31,7 @@ comptime {
     _ = @import("raindrops/test_raindrops.zig");
     _ = @import("rna-transcription/test_rna_transcription.zig");
     _ = @import("sum-of-multiples/test_sum_of_multiples.zig");
+    _ = @import("acronym/acronym.zig");
 }
 
 test {
