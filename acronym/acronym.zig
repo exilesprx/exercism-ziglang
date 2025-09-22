621fb4a729dff75026f5292c7eb2846c2b628fbb
Exiles.prx <exiles.prx@gmail.com>
2025-09-22 16:28:36 -0400

    fix: solved exercise

--- acronym/acronym.zig
+++ acronym/acronym.zig
@@ -2,7 +2,25 @@ const std = @import("std");
 const mem = std.mem;
 
 pub fn abbreviate(allocator: mem.Allocator, words: []const u8) mem.Allocator.Error![]u8 {
-    _ = allocator;
-    _ = words;
-    @compileError("please implement the abbreviate function");
+    var acronym = try std.ArrayList(u8).initCapacity(allocator, words.len);
+    var splitWords = mem.splitAny(u8, words, " -");
+
+    while (splitWords.next()) |word| {
+        if (word.len == 0) {
+            continue;
+        }
+
+        for (word) |char| {
+            switch (char) {
+                'a'...'z', 'A'...'Z' => {
+                    try acronym.append(allocator, std.ascii.toUpper(char));
+                    break;
+                },
+                else => {
+                    continue;
+                },
+            }
+        }
+    }
+    return acronym.toOwnedSlice(allocator);
 }
