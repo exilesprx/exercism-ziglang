9dfd292521152723b0fbb47803982de7fce12e37
Exiles.prx <exiles.prx@gmail.com>
2025-09-23 16:43:32 -0400

    fix: solved exercise resistor color trio

--- resistor-color-trio/resistor_color_trio.zig
+++ resistor-color-trio/resistor_color_trio.zig
@@ -1,8 +1,22 @@
 const std = @import("std");
 const mem = std.mem;
 
+pub const ColorBand = enum { black, brown, red, orange, yellow, green, blue, violet, grey, white };
+
 pub fn label(allocator: mem.Allocator, colors: []const ColorBand) mem.Allocator.Error![]u8 {
-    _ = allocator;
-    _ = colors;
-    @compileError("please implement the label function");
+    const multiplier: usize = std.math.pow(usize, 10, @as(usize, @intFromEnum(colors[2])));
+    const ohms: usize = (10 * @as(usize, @intFromEnum(colors[0])) + @as(usize, @intFromEnum(colors[1]))) * multiplier;
+
+    if (ohms >= 1_000_000_000) {
+        const gigaohms: f64 = @as(f64, @floatFromInt(ohms)) / 1_000_000_000;
+        return try std.fmt.allocPrint(allocator, "{d} gigaohms", .{gigaohms});
+    } else if (ohms >= 1_000_000) {
+        const megaohms: f64 = @as(f64, @floatFromInt(ohms)) / 1_000_000;
+        return try std.fmt.allocPrint(allocator, "{d} megaohms", .{megaohms});
+    } else if (ohms >= 1_000) {
+        const kiloohms: f64 = @as(f64, @floatFromInt(ohms)) / 1_000;
+        return try std.fmt.allocPrint(allocator, "{d} kiloohms", .{kiloohms});
+    } else {
+        return try std.fmt.allocPrint(allocator, "{d} ohms", .{ohms});
+    }
 }
