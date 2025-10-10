4cc5330b56c6dad86b3cb9e4c22621f0f0fd5b25
Exiles.prx <exiles.prx@gmail.com>
2025-10-10 12:36:56 -0400

    fix: solved exercise etl

--- etl/etl.zig
+++ etl/etl.zig
@@ -2,7 +2,16 @@ const std = @import("std");
 const mem = std.mem;
 
 pub fn transform(allocator: mem.Allocator, legacy: std.AutoHashMap(i5, []const u8)) mem.Allocator.Error!std.AutoHashMap(u8, i5) {
-    _ = allocator;
-    _ = legacy;
-    @compileError("please implement the transform function");
+    var map = std.AutoHashMap(u8, i5).init(allocator);
+    try map.ensureTotalCapacity(legacy.capacity());
+
+    var items = legacy.iterator();
+    while (items.next()) |item| {
+        const values = item.value_ptr.*;
+        for (values) |value| {
+            try map.put(std.ascii.toLower(value), item.key_ptr.*);
+        }
+    }
+
+    return map;
 }
