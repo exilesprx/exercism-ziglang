8ee216533b0ca3afc61c9c63e0720ac0affa4ab2
Exiles.prx <exiles.prx@gmail.com>
2025-10-14 14:23:25 -0400

    fix: solved exercise high scores

--- high-scores/high_scores.zig
+++ high-scores/high_scores.zig
@@ -1,23 +1,45 @@
 pub const HighScores = struct {
-    // This struct, as well as its fields and methods, needs to be implemented.
+    highScores: []const i32 = &[_]i32{},
+    best: i32 = 0,
+    topThree: [3]i32 = [_]i32{0} ** 3,
 
     pub fn init(scores: []const i32) HighScores {
-        _ = scores;
-        @compileError("please implement the init method");
+        var best: i32 = 0;
+        var topThree: [3]i32 = [_]i32{0} ** 3;
+        for (scores) |score| {
+            if (score > best) {
+                best = score;
+            }
+
+            if (score > topThree[0]) {
+                topThree[2] = topThree[1];
+                topThree[1] = topThree[0];
+                topThree[0] = score;
+            } else if (score > topThree[1]) {
+                topThree[2] = topThree[1];
+                topThree[1] = score;
+            } else if (score > topThree[2]) {
+                topThree[2] = score;
+            }
+        }
+        return HighScores{ .highScores = scores, .best = best, .topThree = topThree };
     }
 
     pub fn latest(self: *const HighScores) ?i32 {
-        _ = self;
-        @compileError("please implement the latest method");
+        if (self.highScores.len == 0) {
+            return null;
+        }
+        return self.highScores[self.highScores.len - 1];
     }
 
     pub fn personalBest(self: *const HighScores) ?i32 {
-        _ = self;
-        @compileError("please implement the personalBest method");
+        return self.best;
     }
 
     pub fn personalTopThree(self: *const HighScores) []const i32 {
-        _ = self;
-        @compileError("please implement the personalTopThree method");
+        if (self.highScores.len == 0) {
+            return &[_]i32{};
+        }
+        return self.topThree[0..@min(3, self.highScores.len)];
     }
 };
